a3a5c55f903ef83dbb615c37ee8c4feb02d42ae1
CourageAg <xdcourage@gmail.com>
2023-02-16 12:45:56 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - prints the size of various types based on the computer
+* it is compiled and run on ..
+*Return: Always 0.
+*/
+
+int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
+}
